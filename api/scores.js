e50f8836e83bfcfc47df7f325c3a67409ce22bae
Kyle Farwell <m@kfarwell.org>
2023-10-02 03:28:05 -0400

    restrict characters in leaderboard name, remove debug logging

--- api/scores.js
+++ api/scores.js
@@ -2,11 +2,11 @@ import { kv } from '@vercel/kv/dist/index.cjs';
 
 export default async (req, res) => {
   try {
-    const { score, name } = req.body;
+    let { score, name } = req.body;
+
+    name = name.replace(/[^a-zA-Z0-9]/g, '');
 
     let scoresData = await kv.get('scores');
-
-    console.log('Retrieved scoresData:', scoresData);
 
     let scores;
     if (scoresData && typeof scoresData === 'object') {
@@ -17,17 +17,9 @@ export default async (req, res) => {
       scores = [];
     }
 
-    console.log('Parsed scores:', scores);
-
     scores.push({ name, score });
-    console.log('Scores after push:', scores);
-
     scores.sort((a, b) => b.score - a.score);
-    console.log('Scores after sort:', scores);
-
     await kv.set('scores', JSON.stringify(scores));
-    console.log('Scores saved to KV:', JSON.stringify(scores));
-
     res.json({ scores });
   } catch (error) {
     console.error(error);
